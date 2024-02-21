7a60bb57bb874f3bf568e5f0b9347f6d3faccfcc
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2024-02-21 11:54:54 -0800

    add ability to show all or only important

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
     'a new note...'
   )
+  const [showAll, setShowAll] = useState(true)
 
   // event handlers
   const addNote = (event) => {
@@ -23,11 +24,21 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important === true)
+
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
