45b2fcae53bffeb122b9537819e3a8710ae00d4c
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2024-02-21 12:09:22 -0800

    add useEffect hook to get notes from server

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 const App = () => {
@@ -7,6 +8,17 @@ const App = () => {
     'a new note...'
   )
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
   // event handlers
   const addNote = (event) => {
