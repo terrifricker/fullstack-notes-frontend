36e8a4e822190af3fd4831f1639ec9b09eef6346
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2024-02-26 21:26:51 -0800

    add Footer component showing use of react style object

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,19 @@ const Notification = ({ message }) => {
   )
 }
 
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+    </div>
+  )
+}
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -97,6 +110,7 @@ const App = () => {
           onChange={handleNoteChange}/>
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
