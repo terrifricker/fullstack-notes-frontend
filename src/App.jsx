c03a2a0662d0905169bf44dd65cd4dc56fc17c98
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2024-02-26 17:00:39 -0800

    update to noteService.getAll()

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,9 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
+    noteService
+      .getAll()
       .then(response => {
-        console.log('promise fulfilled')
         setNotes(response.data)
       })
   }, [])
